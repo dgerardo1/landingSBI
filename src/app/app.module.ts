014a5dde5c0802b0cc0801594e0e5e5ee251142a
Gerardo Hernandez <gerardo.hernandez@smartbusinesscorp.com>
2021-05-05 20:11:30 -0500

    Inicio de landing SBI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ThanksComponent } from './components/thanks/thanks.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+// Importamos rutas
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ThanksComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( ROUTES )
   ],
   providers: [],
   bootstrap: [AppComponent]
